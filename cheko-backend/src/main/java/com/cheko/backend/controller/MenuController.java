cc983da244721a4a232c6d70bc490c0abcfdc838
basmalbassam <basmalbassam@gmail.com>
2026-03-30 12:36:03 +0300

    add more security

--- cheko-backend/src/main/java/com/cheko/backend/controller/MenuController.java
+++ cheko-backend/src/main/java/com/cheko/backend/controller/MenuController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
-@CrossOrigin
+@CrossOrigin (origins = "http://localhost:3000")
 public class MenuController {
 
     private final MenuService menuService;
