265e990708264b9c3e3e483ac459f6dfe9dc796f
basmalbassam <basmalbassam@gmail.com>
2026-03-30 12:27:54 +0300

    globals.css uses Arial instead

--- cheko-frontend/app/layout.tsx
+++ cheko-frontend/app/layout.tsx
@@ -3,10 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
